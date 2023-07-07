307503dd2775f2e36797b0322c8de683e10759a6
Brendan Boss <bbossdev@gmail.com>
2023-07-07 17:10:28 -0500

    initial roguelike stuff:

---
+++ Assets/Scripts/Roguelike/CombatComponent.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatComponent : MonoBehaviour
+{
+    public uint Health;
+    public uint MaxHealth;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
