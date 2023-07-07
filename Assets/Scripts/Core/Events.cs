c692e144d3dd75ce753d72cedf40cb9dcd84bd46
Dillon Zaugg <dillonzaugg@gmail.com>
2023-07-06 22:59:41 -0500

    Initial commit

---
+++ Assets/Scripts/Core/Events.cs
@@ -0,0 +1,27 @@
+/*
+ *  Pub Sub Event Class Definitions
+ *   - Include a "Usage" comment for each event.
+ */
+
+using UnityEngine;
+
+// Template for a class to use
+// public class SwitchTile
+// {
+//     // Usage: Notifies tiles of a particular color to "switch" (on/off)
+//     public Color color;
+//     public int puzzleId;
+//     public bool isLit;
+//
+//     public SwitchTile(Color _color, int _puzzleId, bool _isLit)
+//     {
+//         color = _color;
+//         puzzleId = _puzzleId;
+//         isLit = _isLit;
+//     }
+// }
+// In Start(), add the subscription like so:
+// switchTileSub = EventBus.Subscribe<SwitchTile>(_OnSwitchTile);
+//
+// Then create the function like so:
+// private void _OnSwitchTile(SwitchTile e) {}
