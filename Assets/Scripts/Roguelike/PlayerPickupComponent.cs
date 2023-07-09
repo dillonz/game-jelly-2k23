169d1e943b4452d9742cf821459a3e34452b2186
LikeTheBossOne <bbossdev@gmail.com>
2023-07-09 01:29:25 -0500

    Adding help text

--- Assets/Scripts/Roguelike/PlayerPickupComponent.cs
+++ Assets/Scripts/Roguelike/PlayerPickupComponent.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(SkillComponent))]
 public class PlayerPickupComponent : MonoBehaviour
 {
     public float DistanceToDetect = 5f;
+    public GameObject HelpText;
 
     private SkillComponent skillComponent;
 
     void Start()
     {
         skillComponent = GetComponent<SkillComponent>();
+        StartCoroutine(CheckForDroppedItems());
     }
     
     void Update()
@@ -29,6 +32,36 @@ public class PlayerPickupComponent : MonoBehaviour
                     break;
                 }
             }
+        }
+    }
+
+    private IEnumerator CheckForDroppedItems()
+    {
+        for (;;)
+        {
+            bool found = false;
+            foreach (GameObject obj in GameObject.FindGameObjectsWithTag("DroppedItems"))
+            {
+                if (Vector2.Distance(obj.transform.position, this.transform.position) < DistanceToDetect)
+                {
+                    found = true;
+                    if (HelpText != null)
+                    {
+                        HelpText.SetActive(true);
+                    }
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                if (HelpText != null)
+                {
+                    HelpText.SetActive(false);
+                }
+            }
+
+            yield return new WaitForSeconds(1f);
         }
     }
 }
