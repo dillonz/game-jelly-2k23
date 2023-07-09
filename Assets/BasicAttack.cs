78a4ff591c8a1eea8f13272ed5c1a689e35c2ce9
Dillon Zaugg <dillonzaugg@gmail.com>
2023-07-08 20:02:11 -0500

    Basic attack respects range

--- Assets/BasicAttack.cs
+++ Assets/BasicAttack.cs
@@ -30,8 +30,9 @@ public class BasicAttack : MonoBehaviour
 
             canAttack = false;
 
-            GameObject go = Instantiate(AttackObject, transform.position + (Vector3)playerMovement.NormalDirection, transform.rotation);
+            GameObject go = Instantiate(AttackObject, transform.position + (Vector3)(playerMovement.NormalDirection * stats.GetRange() / 2 + playerMovement.NormalDirection / 4), transform.rotation);
             go.GetComponent<DoDamageOnStart>().Stats = stats;
+            go.transform.localScale = Vector3.one * stats.GetRange();
             if (playerMovement.FacingLeft) { go.GetComponent<SpriteRenderer>().flipX = true; };
             playerMovement.StopMoving();
             playerMovement.enabled = false;
